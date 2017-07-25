be77a9d951e0e45f65e5e12d89c08b1daf4ae2d8
Saurabh Kumar <28145993+skneo2017@users.noreply.github.com>
2017-07-25 19:05:52 +0530

    Add files via upload

---
+++ rockpapersci.py
@@ -0,0 +1,40 @@
+import random
+class game:
+    human_points=0
+    comp_points =0
+    weapon_of_choice={1:'Rock',2: 'Paper', 3: 'Scissor'}
+    while (human_points!= 5 or comp_points!= 5): 
+        choice=input("enter the choice of your weapon\n Rock - (1)\n Paper - (2)\n scissor - (3)")
+        humanchoice=weapon_of_choice.get(int(choice),"never")
+        compchoice= weapon_of_choice.get(random.randint(1,3),"never")
+        if(humanchoice=='Rock' and compchoice=='Paper'):
+            comp_points+=1
+            print("Point by computer")
+            print(human_points,comp_points)
+        elif(humanchoice=='Paper'and compchoice=='Scissor'):
+            comp_points+=1
+            print("Point by computer")
+            print(human_points,comp_points)
+        elif(humanchoice=='Scissor' and compchoice=='Rock'):
+            comp_points+=1
+            print("Point by computer")
+            print(human_points,comp_points)
+        elif(humanchoice=='Rock'and compchoice=='Scissor'):
+            human_points+=1
+            print("Point by Human")
+            print(human_points,comp_points)
+        elif(humanchoice=='Paper'and compchoice=='Rock'):
+            human_points+=1
+            print("Point by Human")
+            print(human_points,comp_points)
+        elif(humanchoice=='Scissor' and compchoice=='Paper'):
+            human_points+=1
+            print("Point by Human")
+            print(human_points,comp_points)
+        else:
+            print("It is the same")
+            print(human_points,comp_points)
+    if(human_points==5):
+        print("Human WON !!")
+    else:
+        print("Human LOST !!")
