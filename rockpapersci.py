27ad585c06a45445c56e7f86809f1c0f28278617
Saurabh Kumar <28145993+skneo2017@users.noreply.github.com>
2017-07-26 17:55:49 +0530

    Revised one
    It works properly

--- rockpapersci.py
+++ rockpapersci.py
@@ -3,7 +3,7 @@ class game:
     human_points=0
     comp_points =0
     weapon_of_choice={1:'Rock',2: 'Paper', 3: 'Scissor'}
-    while (human_points!= 5 or comp_points!= 5): 
+    while (human_points <5 and comp_points <5): 
         choice=input("enter the choice of your weapon\n Rock - (1)\n Paper - (2)\n scissor - (3)")
         humanchoice=weapon_of_choice.get(int(choice),"never")
         compchoice= weapon_of_choice.get(random.randint(1,3),"never")
