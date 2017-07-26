8bec69790be7ddec46b169329c947082bd2d8484
Saurabh Kumar <28145993+skneo2017@users.noreply.github.com>
2017-07-26 18:47:46 +0530

    New revised one

--- rockpapersci.py
+++ rockpapersci.py
@@ -10,29 +10,36 @@ class game:
         if(humanchoice=='Rock' and compchoice=='Paper'):
             comp_points+=1
             print("Point by computer")
+            print("Human:- "+ humanchoice +"+ computer:-"+ compchoice)
             print(human_points,comp_points)
         elif(humanchoice=='Paper'and compchoice=='Scissor'):
             comp_points+=1
             print("Point by computer")
+            print("Human:- "+ humanchoice +"+ computer:-"+ compchoice)
             print(human_points,comp_points)
         elif(humanchoice=='Scissor' and compchoice=='Rock'):
             comp_points+=1
             print("Point by computer")
+            print("Human:- "+ humanchoice +"+ computer:-"+ compchoice)
             print(human_points,comp_points)
         elif(humanchoice=='Rock'and compchoice=='Scissor'):
             human_points+=1
             print("Point by Human")
+            print("Human:- "+ humanchoice +"+ computer:-"+ compchoice)
             print(human_points,comp_points)
         elif(humanchoice=='Paper'and compchoice=='Rock'):
             human_points+=1
             print("Point by Human")
+            print("Human:- "+ humanchoice +"+ computer:-"+ compchoice)
             print(human_points,comp_points)
         elif(humanchoice=='Scissor' and compchoice=='Paper'):
             human_points+=1
             print("Point by Human")
+            print("Human:- "+ humanchoice +"+ computer:-"+ compchoice)
             print(human_points,comp_points)
         else:
             print("It is the same")
+            print("Human:- "+ humanchoice +"+ computer:-"+ compchoice)
             print(human_points,comp_points)
     if(human_points==5):
         print("Human WON !!")
