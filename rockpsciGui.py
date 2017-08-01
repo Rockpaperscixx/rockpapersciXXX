8362647a87229edf1f14ec3c3a6b4a74e27eff5b
Saurabh Kumar <28145993+skneo2017@users.noreply.github.com>
2017-08-01 18:35:52 +0530

    Add files via upload

--- rockpsciGui.py
+++ rockpsciGui.py
@@ -4,10 +4,6 @@ root=Tk()
 var=StringVar()
 def donothing1(event):
     donothing_1(event)
-def donothing2(event):
-    donothing_2(event)
-def donothing3(event):
-    donothing_3(event)
 frame1=Frame(root,width="300",height="300", bg="green")
 frame1.pack()
 frame1.pack_propagate(0)
@@ -15,7 +11,7 @@ frame2=Frame(frame1, bg="Black")
 frame2.pack(side="bottom",fill=X)
 label2=Label(frame2, height="3", textvariable= var, fg="white", bg="Black")
 label2.pack()
-var.set("asdhgahusdghjags")
+var.set("Result is here")
 label1=Label(frame1, text="Choose your weapon against computer, choose well!!:- ", fg="yellow",bg="green")
 label1.pack(side="top")
 Rock=Button(frame1,text="ROCK",fg="RED", width="5", height="2")
@@ -25,19 +21,11 @@ Rock.pack(side="left")
 Paper.pack(side="right")
 Scissor.pack(side="top")
 Rock.bind('<Button-1>', donothing1)
-Paper.bind('<Button-1>', donothing2)
-Scissor.bind('<Button-1>', donothing3)
+Paper.bind('<Button-1>', donothing1)
+Scissor.bind('<Button-1>', donothing1)
 def donothing_1(event):
     var.set("this is a test")
     choice=1
-    getresult(choice)
-def donothing_2(event):
-    var.set("this is a test")
-    choice=2
-    getresult(choice)
-def donothing_3(event):
-    var.set("this is a test")
-    choice=3
     getresult(choice)
 def getresult(choice):
     human_points=0
